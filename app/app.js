cc2c3a58bc9486cffdafde4da71e206594a84f54
Jason Hamm <jason.hamm@ldschurch.org>
2014-07-02 22:44:39 -0600

    Change templateUrl to template

--- app/app.js
+++ app/app.js
@@ -1,10 +1,23 @@
 require('angular/angular');
+require('angular-route/angular-route');
 require('./bills/bills.module.js');
 
-var app = angular.module('dinero', ['bills']);
+var billsTemplate = require('./bills/bills.html');
+
+console.log(billsTemplate);
+
+var app = angular.module('dinero', ['bills', 'ngRoute']);
+
 app.config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.useXDomain = true;
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/bills', {
+      template: billsTemplate
+    });
 }]);
 
 app.controller('bills', function($scope, billsService) {
