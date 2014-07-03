18fd6653f43d7c565961bb6cb465e1ade1ff895f
Jason Hamm <jason.hamm@ldschurch.org>
2014-07-02 22:54:18 -0600

    Create controller

--- app/bills/bills.module.js
+++ app/bills/bills.module.js
@@ -2,7 +2,8 @@ require('angular/angular');
 require('angular-resource/angular-resource');
 
 var BillsService = require('./bills.service.js');
+var BillsController = require('./bills.controller.js');
 
 var app = angular.module('bills', ['ngResource']);
 app.factory('billsService', ['$resource', '$q', BillsService]);
-
+app.controller('billsController', ['$scope', 'billsService', BillsController]);
