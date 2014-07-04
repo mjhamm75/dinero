81f98c300f35bd9dc90a4fe178e631fc67f4d590
Jason Hamm <jason.hamm@ldschurch.org>
2014-07-04 07:04:40 -0600

    Add watch to log out index of list

--- app/dnd/dnd.controller.js
+++ app/dnd/dnd.controller.js
@@ -1,5 +1,9 @@
-var DndController = function() {
-
+var DndController = function($scope) {
+    $scope.$watch("bills", function(value) {
+        if(value != undefined) {
+            console.log("Model: " + value.map(function(e){return e.id}).join(','));
+        }
+    },true);
 };
 
 module.exports = DndController;
