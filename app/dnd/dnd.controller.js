70b2757abb8dcfe73990f60cccc1c3cbecaa0045
Jason Hamm <jason.hamm@ldschurch.org>
2014-07-09 21:50:38 -0600

    Format code

--- app/dnd/dnd.controller.js
+++ app/dnd/dnd.controller.js
@@ -1,7 +1,9 @@
 var DndController = function($scope) {
     $scope.$watch("bills", function(value) {
-        if(value != undefined) {
-            console.log("Model: " + value.map(function(e){return e.id}).join(','));
+        if(value !== undefined) {
+            console.log("Model: " + value.map(function(e) {
+              return e.id;
+            }).join(','));
         }
     },true);
 };
