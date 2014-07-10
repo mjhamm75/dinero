5832ab7b3dc04d754806a07a8def5f5368c3e8f0
Jason Hamm <jason.hamm@ldschurch.org>
2014-07-09 22:24:13 -0600

    Add directive for multiple lists

--- app/dnd/dnd.module.js
+++ app/dnd/dnd.module.js
@@ -1,6 +1,8 @@
 var DndDirective = require('./dnd.directive.single.js');
+var DndListsDirective = require('./dnd.directive.multiple.js');
 var DndController = require('./dnd.controller.js');
 
 var app = angular.module('dnd', []);
 app.directive('dndList', DndDirective);
+app.directive('dndLists', DndListsDirective);
 app.controller('dndController', DndController);
